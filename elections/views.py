305d28f3dc32acc8f151d5e8c0acacec1a61254c
Daeyeon, Jo <repositivator@gmail.com>
2016-08-22 00:16:53 +0900

    후보 리스트 출력(index.html) 처리

--- elections/views.py
+++ elections/views.py
@@ -5,10 +5,5 @@ from .models import Candidate
 
 def index(request):
     candidates = Candidate.objects.all()
-
-    str = ""
-    for candidate in candidates:
-        str += "<p>{} 기호 {}번<br>".format(candidate.name, candidate.party_number, candidate.area)
-        str += candidate.introduction + "</p>"
-
-    return HttpResponse(str)
+    context = {'candidates':candidates}
+    return render(request, 'elections/index.html', context)
