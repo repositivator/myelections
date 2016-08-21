daf6bbc9f33dd70a29f4bfd66e6f9aa644b32cda
Daeyeon, Jo <repositivator@gmail.com>
2016-08-22 00:56:14 +0900

    투표 결과 출력화면 구현

--- elections/views.py
+++ elections/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import Sum
 from .models import Candidate, Poll, Choice
 import datetime
 
@@ -36,4 +37,33 @@ def polls(request, poll_id):
         choice = Choice(poll_id=poll_id, candidate_id=selection, votes=1)
         choice.save()
 
-    return HttpResponse("finish")
+    return HttpResponseRedirect("/areas/{}/results".format(poll.area))
+
+
+def results(request, area):
+    candidates = Candidate.objects.filter(area=area)
+
+    polls = Poll.objects.filter(area=area)
+    poll_results = []
+
+    for poll in polls:
+        result = {}
+        result['start_date'] = poll.start_date
+        result['end_date'] = poll.end_date
+
+        total_votes = Choice.objects.filter(poll_id=poll.id).aggregate(Sum('votes'))
+        result['total_votes'] = total_votes['votes__sum']
+
+        rates=[]
+        for candidate in candidates:
+            try:
+                choice = Choice.objects.get(poll_id = poll.id, candidate_id = candidate.id)
+                rates.append(round(choice.votes*100/result['total_votes'], 1))
+            except:
+                rates.append(0)
+        result['rates'] = rates
+
+        poll_results.append(result)
+
+    context ={'candidates':candidates, 'area':area, 'poll_results':poll_results}
+    return render(request, 'elections/results.html', context)
