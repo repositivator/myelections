40433851e5c5530c8fbac684f0d4ac9849288f28
Daeyeon, Jo <repositivator@gmail.com>
2016-08-22 00:09:55 +0900

    후보 모델 생성 및 단순 HttpResponse 응답처리

---
+++ elections/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Candidate',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=10)),
+                ('introduction', models.TextField()),
+                ('area', models.CharField(max_length=15)),
+                ('party_number', models.IntegerField(default=0)),
+            ],
+        ),
+    ]
--- elections/views.py
+++ elections/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Candidate
 
-# Create your views here.
+
+def index(request):
+    candidates = Candidate.objects.all()
+
+    str = ""
+    for candidate in candidates:
+        str += "<p>{} 기호 {}번<br>".format(candidate.name, candidate.party_number, candidate.area)
+        str += candidate.introduction + "</p>"
+
+    return HttpResponse(str)
