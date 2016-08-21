d8045691249eec3d97573839528d95f700575529
Daeyeon, Jo <repositivator@gmail.com>
2016-08-22 00:29:17 +0900

    설문조사 화면 출력 with 지역별 후보 정보

--- elections/models.py
+++ elections/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Candidate(models.Model):
     name = models.CharField(max_length=10)
     introduction = models.TextField()
@@ -9,12 +10,20 @@ class Candidate(models.Model):
     def __str__(self):
         return self.name
 
+
 class Poll(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     area = models.CharField(max_length=15)
 
+    def __str__(self):
+        return self.area
+
+
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     candidate = models.ForeignKey(Candidate)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.candidate
